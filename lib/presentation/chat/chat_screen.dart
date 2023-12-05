3d30973eb593bbae13eef6adf1cd8ffc5a201fbd
Maver -- <Ayme Juan>
2023-12-05 16:18:34 -0500

    clase 69

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -10,10 +10,34 @@ class ChatSreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(5.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.UEn6pt4iRHK6eK9N610QtQAAAA?rs=1&pid=ImgDetMain'),
+            backgroundImage: NetworkImage(
+                'https://th.bing.com/th/id/OIP.UEn6pt4iRHK6eK9N610QtQAAAA?rs=1&pid=ImgDetMain'),
           ),
         ),
         title: const Text('Doctor sol'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Text('index: $index');
+              },
+            )),
+            const Text('Mundo')
+          ],
+        ),
       ),
     );
   }
