0ec8a382e2d4c851ba9e61e2b26e8776905d34da
Ayme Juan <Juanayme565@gmail.com>
2023-12-10 15:20:32 -0500

    clase 72

--- lib/presentation/widgets/her_message_bubble.dart
+++ lib/presentation/widgets/her_message_bubble.dart
@@ -25,7 +25,9 @@ class HerMessageBubble extends StatelessWidget {
           height: 5,
         ),
         _ImageBubble(),
-        const SizedBox(height: 10,)
+        const SizedBox(
+          height: 10,
+        )
       ],
     );
   }
@@ -37,13 +39,26 @@ class _ImageBubble extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
-        'https://yesno.wtf/assets/no/25-55dc62642f92cf4110659b3c80e0d7ec.gif',
-        width: size.width*0.8,
-        height: 150,
-        fit: BoxFit.cover,
-        )
-    );
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          'https://yesno.wtf/assets/no/25-55dc62642f92cf4110659b3c80e0d7ec.gif',
+          width: size.width * 0.8,
+          height: 170,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return SizedBox(
+              width: size.width*0.8,
+              height: 170,
+              child: Center(
+                child: CircularProgressIndicator(value: loadingProgress.expectedTotalBytes!=null
+                  ? loadingProgress.cumulativeBytesLoaded/loadingProgress.expectedTotalBytes!
+                  :null,),
+              ),
+            );
+          },
+        ));
   }
 }
